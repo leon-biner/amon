e180d19853294424b86c56a17ff4423862fb0d1b
Leon Biner <bineleon@omicron.gerad.lan>
2025-07-07 13:09:32 -0400

    removed more files + added to .gitignore

--- amon/starting_pts/s.py
+++
@@ -1,7 +0,0 @@
-with open("x5.txt", 'r') as f:
-    lines = f.readlines()
-true_lines = []
-for line in lines:
-    true_lines.append(line.replace(',', ''))
-with open("x5.txt", 'w') as f:
-    f.write(true_lines[0])
